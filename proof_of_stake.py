e55c31c1f0914a3fe517e2e8a9b9c6eb5d66cfa4
Martin Barrios <60498446+Martyb15@users.noreply.github.com>
2026-01-10 13:58:09 -0800

    Validator and ProofOfStake class

--- proof_of_stake.py
+++ proof_of_stake.py
@@ -6,8 +6,18 @@ STAKE_REWARE_PERCENT = 0.02
 
 @dataclass
 class Validator:
-    pass
+    address:        str
+    stake:          int
+    blocks_created: int = 0
 
 class ProofOfStake:
-    pass
+    def __init__(self): 
+        self.validators: Dict[str, Validator] = {}
+        self.balances:   Dict[str, int] = {}
+
+    def set_balance(self, address: str, amount: int): 
+        self.balances[address] = amount
+
+    def stake(self, address: str, amount: int) -> bool: 
+        pass
 
