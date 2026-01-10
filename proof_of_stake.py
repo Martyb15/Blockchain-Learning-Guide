ff4cab1512ea496d4c39d9ad26130289f407ad59
Martin Barrios <60498446+Martyb15@users.noreply.github.com>
2026-01-09 18:20:27 -0800

    Creating Proof of Stake
    imports and classes

---
+++ proof_of_stake.py
@@ -0,0 +1,13 @@
+import random
+from dataclasses import dataclass
+from typing import Dict
+
+STAKE_REWARE_PERCENT = 0.02
+
+@dataclass
+class Validator:
+    pass
+
+class ProofOfStake:
+    pass
+
