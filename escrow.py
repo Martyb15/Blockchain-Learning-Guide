ba8d02f332c016f705cd025f2276e7fb2a75c295
Martin Barrios <60498446+Martyb15@users.noreply.github.com>
2026-01-19 16:56:09 -0800

    Updated escrow.py
    Implemented create_escrow method

--- escrow.py
+++ escrow.py
@@ -17,7 +17,7 @@ class Escrow:
 
 class EscrowSystem: 
     def __init__(self): 
-        self.escrow:         Dict[str, Escrow] = {}
+        self.escrows:         Dict[str, Escrow] = {}
         self.balances:       Dict[str, int]    = {}
         self.current_block = 0
 
@@ -27,8 +27,23 @@ class EscrowSystem:
     def get_balances(self, address: str) -> int:
         return self.balances.get(address, 0)
 
-    def create_escrow(self, sender: str, recipient:str, amount:int):
-        pass
+    def create_escrow(self, sender: str, recipient:str, amount:int, secret_hash: str, timeout_blocks: int = 10) -> Optional[str]:
+        if self.get_balance(sender) < amount:
+            print(f"FAILURE: {sender} has insufficient funds")
+            return None
+        escrow_id = hashlib.sha256(f"{sender}{recipient}{amount}{time.time()}".encode).hexdigest()[:16]
+        self.balances[sender] -= amount 
+        self.escrows[escrow_id] = Escrow(
+            id = escrow_id,
+            sender = sender,
+            recipient = recipient, 
+            amount = amount, 
+            secret_hash = secret_hash,
+            created_at = self.current_block,
+            timeout_blocks = timeout_blocks
+            )
+        print(f"ESCROW CREATED: {escrow_id}")
+        return escrow_id
 
     def claim(self, escrow_id: str, secret: str, claimer: str):
         pass
